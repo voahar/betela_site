63f68914558821bab6cbe5c6e4d1719e695f65e2
voahar <randria.voahar@gmail.com>
2013-08-22 22:34:39 +0200

    refactoring

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -6,8 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 var mongoskin = require('mongoskin');
-var db = mongoskin.db('localhost:27017/beteladb', {safe:true});
-
+//var db = mongoskin.db('localhost:27017/beteladb', {safe:true});
+//var db = mongoskin.db('mongodb://af_betela-randria_voahar:ejenfga7koveej665frdo0nhma@ds039088.mongolab.com:39088/af_betela-randria_voahar', {safe:true});
+//var db = mongoskin.db('mongodb://af_voahar:1178193@ds039088.mongolab.com:39088/af_betela-randria_voahar', {safe:true});
+var db = mongoskin.db('af_voahar:1178193@ds039088.mongolab.com:39088/af_betela-randria_voahar', {safe:true});
+//process.env.MONGOLAB_URI;
 // export models
 exports.posts = new (require('./posts'))(db);
 
